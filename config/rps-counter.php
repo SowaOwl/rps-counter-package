9d5ed1c96fcef0668789fe01abb496d2493a3cd5
Sawa <sovalapati1133@gmail.com>
2025-06-11 12:13:25 +0500

    Fix code style

--- config/rps-counter.php
+++ config/rps-counter.php
@@ -2,5 +2,5 @@
 
 return [
     'redis_key' => env('RPS_COUNTER_REDIS_KEY', 'settings:rps_count'),
-    'redis_prefix' => env('AUTH_REDIS_PREFIX', 'laravel_database_')
+    'redis_prefix' => env('AUTH_REDIS_PREFIX', 'laravel_database_'),
 ];
